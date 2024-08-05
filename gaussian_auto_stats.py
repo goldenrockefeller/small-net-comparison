85fe8fc1ca0fc5044b8edddb1b5c5ec52a37c747
Golden Rockefeller <golden.rockefeller@protonmail.com>
2024-08-05 10:41:14 -0400

    Upload data

--- gaussian_auto_stats.py
+++ gaussian_auto_stats.py
@@ -9,18 +9,18 @@ from pathlib import Path
 
 rng = np.random.default_rng()
 
-n = 30
+n = 60
 
 epsilon = 0.000000001
 
 relu_underspread_model = (
     rng.uniform(-1, 1., size = (n)) ,
-    rng.uniform(-1, 1., size = (n)) ,
+    np.ones(n) ,
     np.array([0.]),
     np.array([0.]),
 )
 
-mu = rng.uniform(0, 1., size = (n)) + 10
+mu = rng.uniform(0, 1., size = (n))
 std = np.ones(n)
 
 
@@ -42,25 +42,44 @@ std = np.ones(n)
 # def restat(std, mu, x_s):
 #     result = scipy.optimize.minimize(get_fstd(mu, x_s), std, method = "CG")
 #     return result.x
+vn = 2
+def get_std_mu(v):
+    nv = 0. *v
+    nv[0] = v[0]
+    nv[1] = v[0] + v[1] ** 2
+    for i in range(len(v)-2):
+        nv[i+2] = nv[i+1] + 2**np.tanh(v[i+2]) * (nv[i+1] - nv[i]) # 2 is a good number for random
+    v = nv
 
+    std = [0.for i in range(len(v)-vn)]
+    mu = [0.for i in range(len(v)-vn)]
 
+    for i in range(len(v)-vn):
+        std[i] =  (v[i+vn] - v[i]) # multiply by 1-4, best is 1
+        mu[i] =  0.5 * (v[i+vn] + v[i])
+
+    return np.array(std), np.array(mu)
 
 def restat(std, mu, x_s):
-    for i in range(100):
-        moment = std * 0.
-        total_activation = std * 0.
+    def cross_entropy(v):
+        new_std, new_mu = get_std_mu(v)
+        entropy = 0
+
         for x in x_s:
-            diff = (x-mu) / std
-            diff_sqr = diff * diff
-            activation = np.exp(-diff_sqr) / std
-            weighted_activation = activation/np.sum(activation)
-            x_sqr = (x-mu) *(x-mu)
-            x_quart = x_sqr * x_sqr
-            total_activation += weighted_activation * 3 * x_sqr
-            moment += 2*x_quart* weighted_activation
-        var = moment/total_activation
-        std = np.sqrt(var)
-    return std
+            activation = np.exp(-0.5 *(new_mu-x)**2/new_std**2)/new_std
+            w = np.sum(activation)
+            entropy -= np.log(w)
+        return entropy
+
+    v = np.zeros(len(std) + vn)
+    v[0] = -20
+    v[1] = 1
+    print(cross_entropy(v))
+    result = scipy.optimize.minimize(cross_entropy, v, method = "CG")
+    print(result.x, "v")
+    print(cross_entropy(result.x), np.sum(result.x), flush = True)
+    std, mu = get_std_mu(result.x)
+    return std, mu
 
 
 
@@ -76,7 +95,7 @@ def eval_model(model, x):
     diff_sqr = diff * diff
     activation = jax.numpy.exp(-diff_sqr)
 
-    return np.sum(activation * (0*m * x + b)) + m1 * x + b1
+    return np.sum(activation * (m * x + b)) + m1 * x + b1
 
 def model_from_vector(vector):
     return (
@@ -124,7 +143,8 @@ def plot_sin(x_s):
 #
 x =  np.hstack(relu_underspread_model)
 x_s = np.arange(0, 1, 0.01)
-std = restat(std, mu, x_s)
+x_s = rng.uniform(0, 1, len(x_s))
+std, mu = restat(std, mu, x_s)
 
 n_epochs_elapsed = 0
 def callback(x):
@@ -138,10 +158,11 @@ y_s = np.sin(6 * np.pi * x_s)
 print(jf(x, x_s, y_s))
 print(jdf(x, x_s, y_s))
 print("JIT Complete")
-#
-# result = scipy.optimize.minimize(f, x, method = "CG", jac = df, callback = callback)
-# x = result.x
-print("Result Complete")
+# #
+old_x = x
+result = scipy.optimize.minimize(f, x, method = "L-BFGS-B", jac = df)
+x = result.x
+# print("Result Complete")
 
 x_s = np.arange(0, 1, 0.001)
 fig = plt.figure()
@@ -149,4 +170,5 @@ fig.suptitle(Path(__file__).name)
 print(Path(__file__).name)
 plot_sin(x_s)
 plot_model(model_from_vector(x), x_s)
+plot_model(model_from_vector(old_x), x_s)
 plt.show()
